cf9f184be3ebc689041bc05b71b774ee82df6bdf
Marco Cusumano-Towner <imarcoam@gmail.com>
2017-04-27 12:21:43 -0400

    A test on forward probabilities passing

--- src/hmm.jl
+++ src/hmm.jl
@@ -1,8 +1,33 @@
+using Distributions
+include("mathutils.jl")
+
 immutable HiddenMarkovModel
-    # NOTE: all must be appropriately normalized
     initial_state_prior::Array{Float64,1}
     transition_model::Array{Float64,2}
     observation_model::Array{Float64,2}
+    num_states::Int
+    num_obs::Int
+
+    function HiddenMarkovModel(initial_state_prior, transition_model, observation_model)
+        if sum(initial_state_prior) != 1.
+            error("initial_state_prior is not normalized: $(initial_state_prior)")
+        end
+        num_states = size(initial_state_prior)[1]
+        if size(transition_model) != (num_states, num_states)
+            error("transition_model has wrong dimension: got $(size(transition_model)) expected $((num_states, num_states))")
+        end
+        if !isapprox(sum(transition_model, 2), ones(num_states, 1))
+            error("transition_model rows are not all normalized: got $(sum(transition_model, 2))")
+        end
+        if size(observation_model)[1] != num_states
+            error("observation_model has wrong number of rows: got $(size(observation_model)[1]) expected $num_states")
+        end
+        num_obs = size(observation_model)[2]
+        if !isapprox(sum(observation_model, 2), ones(num_states, 1))
+            error("observation_model rows are not all normalized: got $(sum(observation_model, 2))")
+        end
+        new(initial_state_prior, transition_model, observation_model, num_states, num_obs)
+    end
 end
 
 function simulate(hmm::HiddenMarkovModel, num_steps::Int)
@@ -17,6 +42,42 @@ function simulate(hmm::HiddenMarkovModel, num_steps::Int)
         observations[i] = rand(Categorical(hmm.observation_model[states[i],:]))
     end
     (states, observations)
+end
+
+function forward_pass(hmm::HiddenMarkovModel, observations::Array{Int, 1})
+    # compute the forward probabilities
+    # p(x_t, y_{1:t-1}) for t=1,...,T
+    # NOTE: we are assuming an observation for each time point
+    num_steps = length(observations)
+    num_states = hmm.num_states
+    fprobs = Array{Float64,2}(num_steps, num_states)
+    fprobs[1,:] = hmm.initial_state_prior
+    for t=2:num_steps
+        obs = observations[t-1]
+        message = fprobs[t-1,:] .* hmm.observation_model[:,obs]
+        # rows are x_{t-1}, cols are x_t
+        product = message' .* hmm.transition_model
+        fprobs[t,:] = vec(sum(product, 1)) # sum over rows
+    end
+    return fprobs
+end
+
+function forward_pass_log_space(hmm::HiddenMarkovModel, observations::Array{Int, 1})
+    # compute the log forward probabilities
+    # log p(x_t, y_{1:t-1}) for t=1,...,T
+    # NOTE: we are assuming an observation for each time point
+    num_steps = length(observations)
+    num_states = hmm.num_states
+    lfprobs = Array{Float64,2}(num_steps, num_states)
+    lfprobs[1,:] = log(hmm.initial_state_prior)
+    for t=2:num_steps
+        obs = observations[t-1]
+        lmessage = lfprobs[t-1,:] .+ log(hmm.observation_model[:,obs])
+        # rows are x_{t-1}, cols are x_t; sum over rows
+        lproduct = lmessage' .+ log(hmm.transition_model)
+        lfprobs[t,:] = logsumexp(lproduct, 1)
+    end
+    return lfprobs
 end
 
 function marginal_likelihood(hmm::HiddenMarkovModel, observations::Array{Int, 1})
@@ -37,9 +98,9 @@ function sample(init::HMMPriorInitializer)
     rand(Categorical(init.hmm.initial_state_prior))
 end
 
-function weight(init::HMMPriorInitializer, x::Int)
-    # the likelihood of the observation given state x
-    pdf(Categorical(init.hmm.observation_model[x,:]), observation)
+function weight(init::HMMPriorInitializer, cur::Int)
+    # the likelihood of the observation given state cur
+    pdf(Categorical(init.hmm.observation_model[cur,:]), observation)
 end
 
 immutable HMMPriorIncrementer
@@ -47,11 +108,43 @@ immutable HMMPriorIncrementer
     observation::Int
 end
 
-function sample(incr::HMMPriorIncrementer, x::Int)
-    rand(Categorical(incr.hmm.transition_model[x,:]))
+function sample(incr::HMMPriorIncrementer, prev::Int)
+    rand(Categorical(incr.hmm.transition_model[prev,:]))
 end
 
-function weight(init::HMMPriorIncrementer, x::Int)
-    pdf(Categorical(init.hmm.observation_model[x,:]), observation)
+function weight(init::HMMPriorIncrementer, prev::Int, cur::Int)
+    pdf(Categorical(init.hmm.observation_model[cur,:]), observation)
 end
 
+immutable HMMConditionalInitializer
+    hmm::HiddenMarkovModel
+    observation::Int
+end
+
+function sample(init::HMMConditionalInitializer)
+    prior = init.hmm.initial_state_prior
+    likelihood = init.hmm.observation_model[:,init.observation]
+    dist = prior .* likelihood
+    dist = init.hmm.initial_state_prior .* init.hmm.observation_modelp\
+    rand(Categorical(dist / sum(dist)))
+end
+
+function weight(init::HMMConditionalInitializer, cur::Int)
+    prior = init.hmm.initial_state_prior
+    likelihood = init.hmm.observation_model[:,init.observation]
+    dist = prior .* likelihood
+    sum(dist) # TODO check me 
+end
+
+immutable HMMConditionalIncrementer
+    hmm::HiddenMarkovModel
+    observation::Int
+end
+
+function sample(incr::HMMConditionalIncrementer, prev::Int)
+    # TODO
+end
+
+function weight(incr::HMMConditionalIncrementer, prev::Int, cur::Int)
+    # TODO
+end
--- src/tests.jl
+++ src/tests.jl
@@ -21,3 +21,19 @@ using Base.Test
     expected = [3.40760596444438 5.593689178496464]'
     @test isapprox(actual, expected)
 end
+
+@testset "hmm tests" begin
+    include("hmm.jl")
+    
+    initial_state_prior = [0.4, 0.6]
+    transition_model = [0.7 0.3; 0.1 0.9]
+    observation_model = [0.6 0.3 0.1; 0.2 0.3 0.5]
+    hmm = HiddenMarkovModel(initial_state_prior, transition_model, observation_model)
+    observations = [1, 2, 3, 3]
+
+    @testset "forward probabilities" begin
+        @test isapprox(forward_pass(hmm, observations), exp(forward_pass_log_space(hmm, observations)))
+
+    end
+
+end
