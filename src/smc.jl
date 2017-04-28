96b0ee1c117bec516a559eb6e97fdb36d6d3707a
Marco Cusumano-Towner <imarcoam@gmail.com>
2017-04-27 20:08:29 -0400

    Remove untested general SMC (add later)

--- src/smc.jl
+++ src/smc.jl
@@ -6,48 +6,6 @@ function resample(log_weights::Array{Float64,1}, n::Int)
     rand(Categorical(dist), n)
 end
 
-immutable GeneralSMCScheme
-    initializer # initial kernel
-    incrementers::Array # (k,l,weight) packages
-    num_particles::Int
-end
-
-function general_smc(scheme::GeneralSMCScheme)
-    num_steps = length(scheme.incrementers)
-    num_particles = scheme.num_particles
-
-    # TODO we don't need to store all of these particles and log_weights
-    # (make it like the state space one, and possibly combine them)
-    particles = Array{Any,2}(num_steps, num_particles)
-    log_weights = Array{Float64,2}(num_steps, num_particles)
-
-    log_ml_estimate::Float64 = 0.0
-
-    # initialize first step particles and log_weights
-    for i=1:num_particles
-        x = sample(scheme.initializer)
-        log_weights[1,i] = log_weight(scheme.initializer, x)
-        particles[1,i] = x
-    end
-    log_ml_estimate += (logsumexp(log_weights[1,:]) - log(num_particles))
-
-    # iterate through steps
-    for t=2:num_steps
-        parents = resample(log_weights[t-1,:], num_particles)
-        for i=1:num_particles
-            prev_x = particles[t-1,parents[i]]
-            new_component = sample(scheme.incrementers[t-1], prev_x)
-            log_weights[t,i] = log_weight(scheme.incrementers[t-1], prev_x, new_component)
-            particles[t,i] = new_component
-        end
-        log_ml_estimate += (logsumexp(log_weights[t,:]) - log(num_particles))
-    end
-
-    output_index = resample(log_weights[end,:], 1)
-    output = particles[end,output_index]
-    (output, log_ml_estimate)
-end
-
 immutable StateSpaceSMCScheme
     initializer
     incrementers::Array
